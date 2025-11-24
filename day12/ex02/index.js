af0eb0b878a0e68c816d20c48113a4854256e1dc
ohy404 <ohy404@gmail.com>
2025-11-24 10:26:22 +0900

    pull

--- day12/ex02/index.js
+++ day12/ex02/index.js
@@ -1,2 +1,26 @@
-export default function main() {
+export default async function main() {
+    console.log("start app");
+
+    let currentInedx = 0;
+    const menuItems = document.querySelectorAll(".menu_text");
+    console.log(menuItems);
+    
+    console.log(menuItems.length);
+    const menuitem_count = menuItems.length;
+
+    menuItems[currentInedx].classList.add("selected");
+    window.addEventListener("keydown", (e) => {
+        console.log(e.key);
+        menuItems[currentInedx].classList.remove("selected");
+        if(e.key == "ArrowUp"){
+            currentInedx--
+            if(currentInedx < 0){
+                currentInedx = menuitem_count - 1;
+            }
+        }
+        if(e.key == "ArrowDown"){
+            currentInedx++
+            currentInedx %= menuitem_count;
+        }
+    });
 }
